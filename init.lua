b3aff29c3ef9fd52803b0d82e6f064a1e2f2872c
Angel Marino <442369+mrangelmarino@users.noreply.github.com>
2025-12-27 23:58:31 -0500

    fix: use new treesitter API (nvim-treesitter.configs deprecated)

--- init.lua
+++ init.lua
@@ -104,32 +104,26 @@ require("lazy").setup({
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "lua",
-          "vim",
-          "vimdoc",
-          "javascript",
-          "typescript",
-          "tsx",
-          "json",
-          "html",
-          "css",
-          "markdown",
-          "bash",
-          "python",
-        },
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-          enable = true,
-        },
-        indent = {
-          enable = true,
-        },
-      })
-    end,
+    main = "nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+        "html",
+        "css",
+        "markdown",
+        "bash",
+        "python",
+      },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
   },
 
   -- Status line (replaces airline)
